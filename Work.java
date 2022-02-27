49be7a636131dfb6a173a6f5e1575ba5f36738df
Artsiom Herasimovich <artsiom_herasimovich@mail.ru>
2022-02-27 17:14:47 +0300

    added homework part1

---
+++ Work.java
@@ -0,0 +1,23 @@
+package jc1_homework;
+
+public class Work {
+    public void task3() {
+        byte b = 0x55;
+        short s = 0x55FF;
+        int i = 1_000_000;
+        long l = 0xFFFFFFFFL;
+        char c = 'a';
+        float f = .25F;
+        double d = .00001234;
+        boolean bool = true;
+
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("l = " + l);
+        System.out.println("c = " + c);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("bool = " + bool);
+    }
+}
