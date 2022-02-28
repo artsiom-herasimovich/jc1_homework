1c92756be10f028e5f160017e821104662481739
Artsiom Herasimovich <artsiom_herasimovich@mail.ru>
2022-02-28 22:39:48 +0300

    added homework part1

---
+++ Main.java
@@ -0,0 +1,39 @@
+package jc1_homework;
+
+import jc1_homework.First;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 3");
+        new First().task3();
+        System.out.println(" ");
+
+        System.out.println("Task 4");
+        System.out.println(new First().task4(7, 3));
+        System.out.println(" ");
+
+        System.out.println("Task 5");
+        new First().task5();
+        System.out.println(" ");
+
+        System.out.println("Task 6");
+        System.out.println(new First().task6(7607));
+        System.out.println(" ");
+
+        System.out.println("Task 7");
+        new First().task7(6, 7, 4);
+        System.out.println(" ");
+
+        System.out.println("Task 8");
+        new First().task8(567);
+        System.out.println(" ");
+
+        System.out.println("Task 9");
+        new First().task9(28, 02, 2022);
+        System.out.println(" ");
+
+        System.out.println("Task 11");
+        new First().task11(7);
+        System.out.println(" ");
+    }
+}
